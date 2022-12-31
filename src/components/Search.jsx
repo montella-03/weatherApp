cac1751f48ad056da8d0cb0f9ecb82acd36ec709
christopher <oigoc60@gmail.com>
2022-12-31 14:38:09 +0300

    hot fix

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,31 +1,43 @@
-import React,{useState} from 'react'
-import { AsyncPaginate } from 'react-select-async-paginate'
+import React, { useState } from "react";
+import { AsyncPaginate } from "react-select-async-paginate";
+import { Url,ApiOptions } from "../Api";
 
-const Search = ({onSearchChange}) => {
-    const [search, setSearch] = useState(null);
-    const loadOptions = async (inputValue) => {
-        try {
-            const response = await fetch(`${Url}/cities?minPopulation=1000000&namePrefix=${inputValue}`, ApiOptions);
-            const response_1 = await response.json();
-            return console.log(response_1);
-        } catch (err) {
-            return console.error(err);
-        }
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
+  const loadOptions = async (inputValue) => {
+    try {
+      const response = await fetch(
+        `${Url}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+        ApiOptions
+      );
+      const response_1 = await response.json();
+      return {
+        options: response_1.data.map((city) => {
+          return {
+            value: `${city.latitude} ${city.longitude}`,
+            label: `${city.name},${city.countryCode}}`,
+          };
+        }),
+      };
+    } catch (err) {
+      return console.error(err);
     }
-    const handleOnChange = (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
+  };
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
   return (
-      <div>
-          <AsyncPaginate
-              placeholder="search for city"
-              debounceTimeout={600}
-              value={search}
-              onChange={handleOnChange}
-              loadOptions={loadOptions } />
+    <div className='text-gray-400 rounded drop-shadow-md'>
+      <AsyncPaginate
+        placeholder="search for city"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Search
+export default Search;
